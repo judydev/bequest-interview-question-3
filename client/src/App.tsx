4155057c7d71829eb06393aecea9bd24466435bd
judydev <judydev23@gmail.com>
2024-12-21 21:14:04 -0800

    save progress: generate hash to compare changes, encrypt/decrypt data before sending to backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,61 @@
 import React, { useEffect, useState } from "react";
+import * as CryptoJS from "crypto-js";
 
 const API_URL = "http://localhost:8080";
 
 function App() {
   const [data, setData] = useState<string>();
+  const [userKey, setUserKey] = useState<string>("");
+  const [timestamp, setTimestamp] = useState<string>("");
+  const [hash, setHash] = useState<string>("");
 
   useEffect(() => {
     getData();
   }, []);
 
   const getData = async () => {
-    const response = await fetch(API_URL);
-    const { data } = await response.json();
+    const { data, timestamp } = await fetchData();
     setData(data);
+    setTimestamp(timestamp);
+  };
+
+  const fetchData = async () => {
+    const response = await fetch(API_URL);
+    const { data, timestamp } = await response.json();
+    const decryptedData = DataService.decryptData(data, userKey);
+    return { data: decryptedData, timestamp };
+  };
+
+  const restoreData = async () => {
+    const response = await fetch(API_URL + "/restore", { method: "PUT" });
+    const { data, timestamp } = await response.json();
+    let decryptedData;
+    try {
+        decryptedData = DataService.decryptData(data, userKey);
+    } catch (error) {
+        alert("Error restoring data");
+        decryptedData = "";
+    }
+    setData(decryptedData);
+    setTimestamp(timestamp);
+
+    // Update hash after restoring data from backup
+    const hash = DataService.generateSHA256Hash(decryptedData);
+    setHash(hash);
   };
 
   const updateData = async () => {
+    const hash = DataService.generateSHA256Hash(data ?? "");
+    setHash(hash);
+
+    const encryptedData = DataService.encryptData(data ?? "", userKey);
+
     await fetch(API_URL, {
       method: "POST",
-      body: JSON.stringify({ data }),
+      body: JSON.stringify({
+        data: encryptedData,
+        timestamp: new Date().toISOString(),
+      }),
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
@@ -29,7 +66,24 @@ function App() {
   };
 
   const verifyData = async () => {
-    throw new Error("Not implemented");
+    const { data: fetchedData } = await fetchData();
+    const fetchedHash = DataService.generateSHA256Hash(fetchedData);
+    const dataHash = DataService.generateSHA256Hash(data ?? "");
+
+    let messages = "";
+    if (dataHash === fetchedHash) {
+      messages += "Frontend data matches backend data.\n";
+    } else {
+      messages += "Frontend data does not match backend data.\n";
+    }
+
+    if (hash === fetchedHash) {
+      messages += "Backend data has not been tampered with.\n";
+    } else {
+      messages += "Backend data has been tampered with.\n";
+    }
+
+    alert(messages);
   };
 
   return (
@@ -55,6 +109,14 @@ function App() {
         onChange={(e) => setData(e.target.value)}
       />
 
+      <div>User Key</div>
+      <input
+        style={{ fontSize: "30px" }}
+        type="text"
+        value={userKey}
+        onChange={(e) => setUserKey(e.target.value)}
+      />
+
       <div style={{ display: "flex", gap: "10px" }}>
         <button style={{ fontSize: "20px" }} onClick={updateData}>
           Update Data
@@ -62,9 +124,35 @@ function App() {
         <button style={{ fontSize: "20px" }} onClick={verifyData}>
           Verify Data
         </button>
+        <button style={{ fontSize: "20px" }} onClick={restoreData}>
+          Restore Data
+        </button>
       </div>
+
+      <div>Last Updated At: {timestamp}</div>
+      <div>Hash: {hash}</div>
     </div>
   );
 }
 
 export default App;
+
+class DataService {
+  static encryptData(data: string, userKey: string): string {
+    return CryptoJS.AES.encrypt(data, userKey).toString();
+  }
+
+  static decryptData(data: string, userKey: string): string {
+    try {
+        const bytes = CryptoJS.AES.decrypt(data, userKey);
+        return bytes.toString(CryptoJS.enc.Utf8);
+    } catch (error) {
+        console.error("Decryption failed:", error);
+        throw error;
+    }
+  }
+
+  static generateSHA256Hash(data: string): string {
+    return CryptoJS.SHA256(data).toString(CryptoJS.enc.Hex);
+  }
+}
--- server/app.ts
+++ server/app.ts
@@ -3,7 +3,8 @@ import cors from "cors";
 
 const PORT = 8080;
 const app = express();
-const database = { data: "Hello World" };
+const database = { data: "Hello World", timestamp: "2024-12-21T00:00:00Z" };
+const backup = { data: "", timestamp: "" };
 
 app.use(cors());
 app.use(express.json());
@@ -14,8 +15,25 @@ app.get("/", (req, res) => {
   res.json(database);
 });
 
+app.get("/backup", (req, res) => {
+  res.json(backup);
+});
+
+app.put("/restore", (req, res) => {
+  database.data = backup.data;
+  database.timestamp = new Date().toISOString();
+
+  backup.timestamp = new Date().toISOString();
+  res.json(database);
+});
+
 app.post("/", (req, res) => {
+  // Write existing data to backup
+  backup.data = database.data; 
+
+  // Write new data to database
   database.data = req.body.data;
+  database.timestamp = req.body.timestamp;
   res.sendStatus(200);
 });
 
